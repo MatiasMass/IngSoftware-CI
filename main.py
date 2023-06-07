83e3f4d4b33f06c537b382efc913cc44bb123acf
MatiasMass <matiasmass14@gmail.com>
2023-06-07 18:57:02 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from . import api
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api.router, tags=["api"], prefix="/api/products")
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI is working"}
---
+++ test.py
@@ -0,0 +1,60 @@
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message":  "FastAPI is working"}
+
+
+def test_get_products():
+    response = client.get("/api/products/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "Status": "Success",
+        "Results": 2,
+        "Products":     [
+            {
+                "id": 1,
+                "name": "Product 1",
+                "description": "This is the description for product 1",
+                "price": 100,
+                "image": "https://picsum.photos/200/300"
+            },
+            {
+                "id": 2,
+                "name": "Product 2",
+                "description": "This is the description for product 2",
+                "price": 200,
+                "image": "https://picsum.photos/200/300"
+            },
+        ],
+    }
+
+
+def test_get_product():
+    response = client.get("/api/products/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "Status": "Success",
+        "Product":     {
+            "id": 1,
+            "name": "Product 1",
+            "description": "This is the description for product 1",
+            "price": 100,
+            "image": "https://picsum.photos/200/300"
+        },
+    }
+
+
+def test_get_user_not_found():
+    response = client.get(
+        f"/api/products/4"
+    )
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "No Product with this id: `4` found"
+
+    }
