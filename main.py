cdb50413cc1eb78507b50fb1be90d008715403e7
MatiasMass <matiasmass14@gmail.com>
2023-06-08 21:54:43 -0300

    some changes

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ app.include_router(router, tags=["api"], prefix="/api/products")
 
 @app.get("/")
 def root():
-    return {"message": "FastAPI is not working!! Try again"}
+    return {"message": "FastAPI is working"}
