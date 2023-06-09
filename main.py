9510da2c00b8838c6cc2e7886a76369af3480a8d
MatiasMass <matiasmass14@gmail.com>
2023-06-08 21:33:39 -0300

    unit test 6

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ app.include_router(router, tags=["api"], prefix="/api/products")
 
 @app.get("/")
 def root():
-    return {"message": "FastAPI is not working!! hello world!! 232323"}
+    return {"message": "FastAPI is not working!! hello world!! 232323 hola mundo"}
