8d9db3aa159e7a7cfb9d0931a5641119891f3df9
MatiasMass <matiasmass14@gmail.com>
2023-06-10 20:28:47 -0300

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+import json
+import uvicorn
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from api import router
-
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="static")
 
 origins = [
     "http://localhost:8000",
@@ -18,8 +23,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(router, tags=["api"], prefix="/api/products")
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI is working"}
+@app.get("/", response_class=HTMLResponse, status_code=200)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "FastAPI is working"})
+
+   
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    
